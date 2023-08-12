15d7a26f9dc55619b76d70a2dcfd887a3595d74a
aguramirez <agustinramirez58@gmail.com>
2023-08-12 18:42:57 -0300

    primer commit

---
+++ src/main/java/com/agustin/concesionariabackend/concesionariabackend/ConcesionariabackendApplication.java
@@ -0,0 +1,13 @@
+package com.agustin.concesionariabackend.concesionariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionariabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionariabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agustin/concesionariabackend/concesionariabackend/ConcesionariabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustin.concesionariabackend.concesionariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionariabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
